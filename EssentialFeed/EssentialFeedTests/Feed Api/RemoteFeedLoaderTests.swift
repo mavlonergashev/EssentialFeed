5b3ab1e729a9dde839e6e072bfba0dd774bac917
mavlon <mavlon@zeroone.uz>
2025-03-13 10:33:07 +0100

    Remove `LoadFeedResult` Equatable conformance from production since it's a test-only requirement

--- EssentialFeed/EssentialFeedTests/Feed Api/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Api/RemoteFeedLoaderTests.swift
@@ -171,17 +171,29 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private func expect(
         _ sut: RemoteFeedLoader,
-        toCompleteWithError result: RemoteFeedLoader.Result,
+        toCompleteWithError expectedResult: RemoteFeedLoader.Result,
         when action: () -> Void,
         file: StaticString = #filePath,
         line: UInt = #line
     ) {
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load { capturedResults.append($0) }
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedItems), .success(expectedItems)):
+                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+            default:
+                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
         
         action()
         
-        XCTAssertEqual(capturedResults, [result], file: file, line: line)
+        wait(for: [exp], timeout: 1.0)
     }
     
     private class HttpClientSpy: HTTPClient {
