c21084bb7b89c419a0eae8f2194752b8821e0d86
mavlon <mavlon@zeroone.uz>
2025-03-29 13:18:23 +0100

    Add memory leak tracking

--- EssentialFeed/EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
@@ -76,8 +76,25 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     //MARK: -Helpers
     
-    private func makeSUT() -> URLSesssionHTTPClient {
-        return URLSesssionHTTPClient()
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSesssionHTTPClient {
+        let sut = URLSesssionHTTPClient()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func trackForMemoryLeaks(
+        _ instance: AnyObject,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(
+                instance,
+                "Instance should have been deallocated. Potential memory leak.",
+                file: file,
+                line: line
+            )
+        }
     }
 
     private class URLProtocolStub: URLProtocol {
